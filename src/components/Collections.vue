e8e21494f7d977d0236e244b36acfd183ab72629
ore291 <oreoluwapadonu@gmail.com>
2022-01-14 07:40:29 +0100

    .

--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -6,53 +6,53 @@
       <p class="text-xl font-medium text-text">Collections</p >
     </div>
     <div class="grid grid-cols-4 sm:grid-cols-6 grid-rows-2  place-items-center sm:gap-1 sm:my-2">
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/connect-your-world_260x144.png" alt="">
-            <p class="collectionCard">Connect your World</p >
+            <p class="collectionCardText">Connect your World</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/dress-to-impress_260x144.png" alt="">
-            <p class="collectionCard">Dress to Impress</p >
+            <p class="collectionCardText">Dress to Impress</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/supermarket_260x144.png" alt="">
-            <p class="collectionCard">Eat Healthier</p >
+            <p class="collectionCardText">Eat Healthier</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/family-time_260x144.png" alt="">
-            <p class="collectionCard">Family Time</p >
+            <p class="collectionCardText">Family Time</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/the-next-entrepreneur_260x144.png" alt="">
-            <p class="collectionCard">Next Entreprenuer</p >
+            <p class="collectionCardText">Next Entreprenuer</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/pickup-new-hobby_260x144.png" alt="">
-            <p class="collectionCard">New Hobby</p >
+            <p class="collectionCardText">New Hobby</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/mens-fashion_260x144.png" alt="">
-            <p class="collectionCard"><del>&#8358;</del>5,000 Store</p >
+            <p class="collectionCardText"><del>&#8358;</del>5,000 Store</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/home-appliances_260x144.png" alt="">
-            <p class="collectionCard">Half Price Store</p >
+            <p class="collectionCardText">Half Price Store</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/accessories-warehouse_260x144v2.png" alt="">
-            <p class="collectionCard">Accessories</p >
+            <p class="collectionCardText">Accessories</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/electronics_260x144.png" alt="">
-            <p class="collectionCard">Exclusive Deals</p >
+            <p class="collectionCardText">Exclusive Deals</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/get-in-shape_260x144.png" alt="">
-            <p class="collectionCard">Get in Shape</p >
+            <p class="collectionCardText">Get in Shape</p >
         </div>
-        <div class="flex flex-col items-center justify-center transform transition duration-[50ms] hover:scale-[1.02] hover:shadow-xl">
+        <div class="collectionCard">
             <img src="../assets/the-good-times_260x144.png" alt="">
-            <p class="collectionCard">The Good Times</p >
+            <p class="collectionCardText">The Good Times</p >
         </div>
         
     </div>
